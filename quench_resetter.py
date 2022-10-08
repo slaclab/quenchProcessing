5e8ccc3f78f4a2af64281df4a4cf9ec40cd640de
Lisa Zacarias <zacarias@slac.stanford.edu>
2022-10-08 14:43:34 -0700

    adding sanity check print

--- quench_resetter.py
+++ quench_resetter.py
@@ -3,6 +3,7 @@ from quench_linac import QUENCH_CRYOMODULES
 while True:
     for quench_cm in QUENCH_CRYOMODULES:
         for quench_cav in quench_cm.cavities.values:
+            print(f"Checking {quench_cav}")
             if quench_cav.quench_latch_pv.value == 1:
                 is_real = quench_cav.validate_quench(wait_for_update=True)
                 if not is_real:
