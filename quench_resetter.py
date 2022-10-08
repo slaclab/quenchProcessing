160b650528a9d552bfa32c6ae85ebe6426d10da4
Lisa Zacarias <zacarias@slac.stanford.edu>
2022-10-08 15:05:49 -0700

    storing PV objects for better timeout handling

--- quench_resetter.py
+++ quench_resetter.py
@@ -5,7 +5,7 @@ from quench_linac import QUENCH_CRYOMODULES
 while True:
     for cryomoduleName in ALL_CRYOMODULES:
         quench_cm = QUENCH_CRYOMODULES[cryomoduleName]
-        for quench_cav in quench_cm.cavities.values:
+        for quench_cav in quench_cm.cavities.values():
             print(f"Checking {quench_cav}")
             if quench_cav.quench_latch_pv.value == 1:
                 is_real = quench_cav.validate_quench(wait_for_update=True)
