e2d58a6d62622817413cbf9436feab704e98bc3a
Lisa Zacarias <zacarias@slac.stanford.edu>
2023-05-26 15:32:46 -0700

    adding a file logger and removing the check for bypassed interlock now that it's no longer regularly used

--- quench_resetter.py
+++ quench_resetter.py
@@ -1,5 +1,7 @@
+import logging
+
 from epics import PV
-from lcls_tools.superconducting.scLinac import ALL_CRYOMODULES
+from lcls_tools.superconducting.scLinac import ALL_CRYOMODULES, Cryomodule
 from numpy.linalg import LinAlgError
 
 from quench_linac import QUENCH_CRYOMODULES
@@ -7,23 +9,29 @@ from quench_linac import QUENCH_CRYOMODULES
 WATCHER_PV: PV = PV("PHYS:SYS0:1:SC_CAV_QNCH_RESET_HEARTBEAT")
 WATCHER_PV.put(0)
 
+logger = logging.getLogger('srf_quench_resetter')
+logger.setLevel(logging.DEBUG)
+fh = logging.FileHandler('srf_quench_resetter.log')
+fh.setLevel(logging.DEBUG)
+logger.addHandler(fh)
+
 while True:
     for cryomoduleName in ALL_CRYOMODULES:
-        quench_cm = QUENCH_CRYOMODULES[cryomoduleName]
+        quench_cm: Cryomodule = QUENCH_CRYOMODULES[cryomoduleName]
         for quench_cav in quench_cm.cavities.values():
-            if quench_cav.quench_intlk_bypassed:
+            if quench_cav.quench_latch_pv.value == 1:
                 try:
-                    if quench_cav.selAmplitudeActPV.get() < 0.9 * quench_cav.selAmplitudeDesPV.get():
-                        print(f"{quench_cav} quench interlock bypassed and AACT < 0.9*ADES, turning off")
-                        quench_cav.setPowerState(turnOn=False, wait_time=0)
-                except TypeError as e:
-                    print(f"{quench_cav} error: {e}")
-            else:
-                if quench_cav.quench_latch_pv.value == 1:
-                    try:
-                        is_real = quench_cav.validate_quench(wait_for_update=True)
-                        if not is_real:
-                            quench_cav.reset_interlocks(wait=False, retry=False)
-                    except(TypeError, LinAlgError, IndexError) as e:
-                        print(f"{quench_cav} error:", e)
+                    is_real = quench_cav.validate_quench(wait_for_update=True)
+                    
+                    if not is_real:
+                        logger.info(f"{quench_cav} FAKE quench detected, resetting")
+                        quench_cav.reset_interlocks(wait=False, retry=False)
+                    
+                    else:
+                        logger.warning(f"{quench_cav} REAL quench detected, not resetting")
+                
+                except(TypeError, LinAlgError, IndexError) as e:
+                    logger.error(f"{quench_cav} error: {e}")
+                    print(f"{quench_cav} error:", e)
+    
     WATCHER_PV.put(WATCHER_PV.get() + 1)
