d413cd26859286e0b142be1c928f2e0b3286e3c6
Lisa Zacarias <zacarias@slac.stanford.edu>
2023-10-24 12:57:47 -0700

    refactoring

--- quench_gui.py
+++ quench_gui.py
@@ -3,8 +3,9 @@ from typing import Dict
 
 from PyQt5.QtCore import Qt
 from epics import camonitor, camonitor_clear
-from lcls_tools.common.pydm_tools import pydmPlotUtil
-from lcls_tools.superconducting.scLinac import ALL_CRYOMODULES, Cryomodule
+import lcls_tools.common.frontend.plotting.util as pydmPlotUtil
+from lcls_tools.superconducting.sc_linac_utils import ALL_CRYOMODULES
+from lcls_tools.superconducting.scLinac import Cryomodule
 from pydm import Display
 from qtpy.QtCore import Signal, Slot
 
@@ -13,95 +14,121 @@ from quench_linac import QUENCH_CRYOMODULES, QuenchCavity
 
 class QuenchGUI(Display):
     quench_signal = Signal(int)
-    
+
     def __init__(self, parent=None, args=None):
         super().__init__(parent=parent, args=args)
-        
+
         self.ui.cm_combobox.addItems(ALL_CRYOMODULES)
         self.ui.cav_combobox.addItems(map(str, range(1, 9)))
-        
+
         self.current_cm = None
         self.current_cav = None
-        
+
         self.waveform_plot_params: Dict[str, pydmPlotUtil.WaveformPlotParams] = {
-            "FAULT_WAVEFORMS": pydmPlotUtil.WaveformPlotParams(plot=self.ui.cav_waveform_plot)}
-        
+            "FAULT_WAVEFORMS": pydmPlotUtil.WaveformPlotParams(
+                plot=self.ui.cav_waveform_plot
+            )
+        }
+
         self.timeplot_params: Dict[str, pydmPlotUtil.TimePlotParams] = {
-            "LIVE_SIGNALS": pydmPlotUtil.TimePlotParams(plot=self.ui.amp_rad_timeplot)}
-        
-        self.timeplot_updater: pydmPlotUtil.TimePlotUpdater = pydmPlotUtil.TimePlotUpdater(self.timeplot_params)
-        self.waveform_updater: pydmPlotUtil.WaveformPlotUpdater = pydmPlotUtil.WaveformPlotUpdater(
-                self.waveform_plot_params)
-        
+            "LIVE_SIGNALS": pydmPlotUtil.TimePlotParams(plot=self.ui.amp_rad_timeplot)
+        }
+
+        self.timeplot_updater: pydmPlotUtil.TimePlotUpdater = (
+            pydmPlotUtil.TimePlotUpdater(self.timeplot_params)
+        )
+        self.waveform_updater: pydmPlotUtil.WaveformPlotUpdater = (
+            pydmPlotUtil.WaveformPlotUpdater(self.waveform_plot_params)
+        )
+
         self.update_cm()
-        
+
         self.ui.cm_combobox.currentIndexChanged.connect(self.update_cm)
         self.ui.cav_combobox.currentIndexChanged.connect(self.update_cm)
         self.quench_signal.connect(self.quench_slot, Qt.QueuedConnection)
-    
+
     def clear_connections(self, signal: Signal):
         try:
             signal.disconnect()
         except TypeError:
             print(f"No connections to remove for {signal}")
             pass
-    
+
     def clear_all_connections(self):
-        for signal in [self.ui.button_ssa_on.clicked,
-                       self.ui.button_ssa_off.clicked,
-                       self.ui.button_rf_on.clicked,
-                       self.ui.button_rf_off.clicked,
-                       self.ui.setup_button.clicked,
-                       self.ui.reset_button.clicked]:
+        for signal in [
+            self.ui.button_ssa_on.clicked,
+            self.ui.button_ssa_off.clicked,
+            self.ui.button_rf_on.clicked,
+            self.ui.button_rf_off.clicked,
+            self.ui.setup_button.clicked,
+            self.ui.reset_button.clicked,
+        ]:
             self.clear_connections(signal)
-    
+
     def update_cm(self):
         if self.current_cav:
             camonitor_clear(self.current_cav.quench_latch_pv)
-        
+
         self.clear_all_connections()
-        
-        self.current_cm: Cryomodule = QUENCH_CRYOMODULES[self.ui.cm_combobox.currentText()]
-        self.current_cav: QuenchCavity = self.current_cm.cavities[int(self.ui.cav_combobox.currentText())]
-        
+
+        self.current_cm: Cryomodule = QUENCH_CRYOMODULES[
+            self.ui.cm_combobox.currentText()
+        ]
+        self.current_cav: QuenchCavity = self.current_cm.cavities[
+            int(self.ui.cav_combobox.currentText())
+        ]
+
         self.ui.button_ssa_on.clicked.connect(self.current_cav.ssa.turnOn)
         self.ui.button_ssa_off.clicked.connect(self.current_cav.ssa.turnOff)
         self.ui.label_ssa_status_rdbk.channel = self.current_cav.ssa.statusPV
-        
-        self.ui.combobox_rfmode.channel = self.current_cav.rfModeCtrlPV.pvname
-        self.ui.label_rfmode_rdbk.channel = self.current_cav.rfModePV.pvname
-        self.ui.button_rf_on.clicked.connect(self.current_cav.turnOn)
+
+        self.ui.combobox_rfmode.channel = self.current_cav.rf_control_pv
+        self.ui.label_rfmode_rdbk.channel = self.current_cav.rf_mode_pv
+        self.ui.button_rf_on.clicked.connect(self.current_cav.turn_on())
         self.ui.button_rf_off.clicked.connect(self.current_cav.turnOff)
-        self.ui.label_rfstatus_rdbk.channel = self.current_cav.rfStatePV.pvname
-        self.ui.ades_spinbox.channel = self.current_cav.selAmplitudeDesPV.pvname
-        self.ui.ades_readback_label.channel = self.current_cav.selAmplitudeActPV.pvname
-        self.ui.setup_button.clicked.connect(self.current_cav.setup_SELA)
+        self.ui.label_rfstatus_rdbk.channel = self.current_cav.rf_state_pv
+        self.ui.ades_spinbox.channel = self.current_cav.ades_pv
+        self.ui.ades_readback_label.channel = self.current_cav.aact_pv
+        self.ui.setup_button.clicked.connect(self.current_cav.setup_sela)
         self.ui.srf_max_spinbox.channel = self.current_cav.srf_max_pv
         self.ui.srf_max_label.channel = self.current_cav.srf_max_pv
-        
+
         self.ui.cav_power_label.channel = self.current_cav.cav_power_pv
         self.ui.forward_power_label.channel = self.current_cav.forward_power_pv
         self.ui.rev_power_label.channel = self.current_cav.reverse_power_pv
-        
+
         self.ui.latch_indicator.channel = self.current_cav.quench_latch_pv
         self.ui.latch_label.channel = self.current_cav.quench_latch_pv
-        
+
         self.ui.bypass_button.channel = self.current_cav.quench_bypass_pv
         self.ui.unbypass_button.channel = self.current_cav.quench_bypass_pv
         self.ui.bypass_indicator.channel = self.current_cav.quench_bypass_pv + "_RBV"
         self.ui.bypass_label.channel = self.current_cav.quench_bypass_pv + "_RBV"
-        
-        self.ui.reset_button.clicked.connect(partial(self.current_cav.reset_interlocks, True))
-        
-        self.timeplot_updater.updatePlot("LIVE_SIGNALS", [(self.current_cav.selAmplitudeActPV.pvname, None)])
-        self.waveform_updater.updatePlot("FAULT_WAVEFORMS", [(self.current_cav.cav_time_waveform_pv,
-                                                              self.current_cav.decay_ref_pv),
-                                                             (self.current_cav.cav_time_waveform_pv,
-                                                              self.current_cav.fault_waveform_pv)])
-        
-        camonitor(self.current_cav.quench_latch_pv,
-                  callback=self.quench_callback, writer=print)
-    
+
+        self.ui.reset_button.clicked.connect(
+            partial(self.current_cav.reset_interlocks, True)
+        )
+
+        self.timeplot_updater.updatePlot(
+            "LIVE_SIGNALS", [(self.current_cav.aact_pv, None)]
+        )
+        self.waveform_updater.updatePlot(
+            "FAULT_WAVEFORMS",
+            [
+                (self.current_cav.cav_time_waveform_pv, self.current_cav.decay_ref_pv),
+                (
+                    self.current_cav.cav_time_waveform_pv,
+                    self.current_cav.fault_waveform_pv,
+                ),
+            ],
+        )
+
+        camonitor(
+            self.current_cav.quench_latch_pv,
+            callback=self.quench_callback,
+            writer=print,
+        )
+
     @Slot(int)
     def quench_slot(self, value: int):
         is_real = self.current_cav.validate_quench(wait_for_update=True)
@@ -113,12 +140,12 @@ class QuenchGUI(Display):
             self.ui.valid_label.setText("Not Real")
             if self.ui.auto_reset_fake_checkbox.isChecked():
                 self.current_cav.reset_interlocks(True)
-        
+
         if self.ui.auto_reset_all_checkbox.isChecked():
             self.current_cav.reset_interlocks(True)
-    
+
     def quench_callback(self, value, **kwargs):
         self.quench_signal.emit(value)
-    
+
     def ui_filename(self):
         return "quench_gui.ui"
--- quench_linac.py
+++ quench_linac.py
@@ -2,15 +2,21 @@ from datetime import datetime
 from time import sleep
 
 import numpy as np
-from lcls_tools.common.pyepics_tools.pyepics_utils import PV
+from lcls_tools.common.controls.pyepics.utils import PV
 from lcls_tools.superconducting import scLinac
 
 LOADED_Q_CHANGE_FOR_QUENCH = 0.6
 
 
 class QuenchCavity(scLinac.Cavity):
-    def __init__(self, cavityNum, rackObject, ssaClass=scLinac.SSA,
-                 stepperClass=scLinac.StepperTuner, piezoClass=scLinac.Piezo):
+    def __init__(
+        self,
+        cavityNum,
+        rackObject,
+        ssaClass=scLinac.SSA,
+        stepperClass=scLinac.StepperTuner,
+        piezoClass=scLinac.Piezo,
+    ):
         super().__init__(cavityNum, rackObject)
         self.cav_power_pv = self.pv_addr("CAV:PWRMEAN")
         self.forward_power_pv = self.pv_addr("FWD:PWRMEAN")
@@ -22,61 +28,61 @@ class QuenchCavity(scLinac.Cavity):
         self.pre_quench_amp = None
         self._quench_bypass_rbck_pv: PV = None
         self._current_q_loaded_pv_obj: PV = None
-    
+
     @property
     def current_q_loaded_pv_obj(self):
         if not self._current_q_loaded_pv_obj:
             self._current_q_loaded_pv_obj = PV(self.current_q_loaded_pv)
         return self._current_q_loaded_pv_obj
-    
+
     @property
     def hw_mode_pv_obj(self) -> PV:
         if not self._hw_mode_pv_obj:
             self._hw_mode_pv_obj = PV(self.hw_mode_pv)
         return self._hw_mode_pv_obj
-    
+
     @property
     def hw_mode(self):
         return self.hw_mode_pv_obj.get()
-    
+
     @property
     def quench_latch_pv_obj(self) -> PV:
         if not self._quench_latch_pv_obj:
             self._quench_latch_pv_obj = PV(self.quench_latch_pv)
         return self._quench_latch_pv_obj
-    
+
     @property
     def quench_intlk_bypassed(self) -> bool:
         if not self._quench_bypass_rbck_pv:
             self._quench_bypass_rbck_pv = PV(self.pv_addr("QUENCH_BYP_RBV"))
         return self._quench_bypass_rbck_pv.get() == 1
-    
+
     @property
     def fault_waveform_pv(self) -> PV:
         if not self._fault_waveform_pv:
             self._fault_waveform_pv = PV(self.pv_addr("CAV:FLTAWF"))
         return self._fault_waveform_pv
-    
+
     @property
     def cav_time_waveform_pv(self) -> PV:
         if not self._cav_time_waveform_pv:
             self._cav_time_waveform_pv = PV(self.pv_addr("CAV:FLTTWF"))
         return self._cav_time_waveform_pv
-    
+
     def reset_interlocks(self, wait: int = 0, attempt: int = 0):
         """Overwriting base function to skip wait/reset cycle"""
         print(f"Resetting interlocks for {self}")
-        
+
         if not self._interlock_reset_pv_obj:
             self._interlock_reset_pv_obj = PV(self.interlock_reset_pv)
-        
+
         self._interlock_reset_pv_obj.put(1)
-    
+
     def validate_quench(self, wait_for_update: bool = False):
         """
         Parsing the fault waveforms to calculate the loaded Q to try to determine
         if a quench was real.
-        
+
         DERIVATION NOTES
         A(t) = A0 * e^((-2 * pi * cav_freq * t)/(2 * loaded_Q)) = A0 * e ^ ((-pi * cav_freq * t)/loaded_Q)
 
@@ -85,54 +91,56 @@ class QuenchCavity(scLinac.Cavity):
         polyfit(t, ln(A0/A(t)), 1) = [(pi * f * t)/Ql]
 
         https://education.molssi.org/python-data-analysis/03-data-fitting/index.html
-        
+
         :param wait_for_update: bool
         :return:
         """
-        
+
         if wait_for_update:
             print(f"Waiting 0.1s to give {self} waveforms a chance to update")
             sleep(0.1)
-        
+
         time_data = self.cav_time_waveform_pv.value
         fault_data = self.fault_waveform_pv.value
         time_0 = 0
-        
+
         # Look for time 0 (quench). These waveforms capture data beforehand
         for time_0, time in enumerate(time_data):
             if time >= 0:
                 break
-        
+
         fault_data = fault_data[time_0:]
         time_data = time_data[time_0:]
-        
+
         end_decay = len(fault_data) - 1
-        
+
         # Find where the amplitude decays to "zero"
         for end_decay, amp in enumerate(fault_data):
             if amp < 0.002:
                 break
-        
+
         fault_data = fault_data[:end_decay]
         time_data = time_data[:end_decay]
-        
+
         saved_loaded_q = self.current_q_loaded_pv_obj.get()
-        
+
         self.pre_quench_amp = fault_data[0]
-        
-        exponential_term = np.polyfit(time_data, np.log(self.pre_quench_amp / fault_data), 1)[0]
+
+        exponential_term = np.polyfit(
+            time_data, np.log(self.pre_quench_amp / fault_data), 1
+        )[0]
         loaded_q = (np.pi * self.frequency) / exponential_term
-        
+
         thresh_for_quench = LOADED_Q_CHANGE_FOR_QUENCH * saved_loaded_q
         print(f"\nCM{self.cryomodule.name}", f"Cavity {self.number}", datetime.now())
         print("Saved Loaded Q: ", "{:e}".format(saved_loaded_q))
         print("Last recorded amplitude: ", fault_data[0])
         print("Threshold: ", "{:e}".format(thresh_for_quench))
         print("Calculated Loaded Q: ", "{:e}\n".format(loaded_q))
-        
+
         is_real = loaded_q < thresh_for_quench
         print("Validation: ", is_real)
-        
+
         return is_real
 
 
